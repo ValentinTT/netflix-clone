a55890c2f115d650bdfe630ae975d239b9b91c04
ValentinTT <valentintapiatorti@gmail.com>
2021-09-30 22:16:34 -0300

    Update globalStyle.ts

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -3,6 +3,7 @@ import Colors from "styles/Colors";
 import { fontsCSS } from "fonts/fonts";
 
 const GlobalStyle = createGlobalStyle`
+  ${fontsCSS}
   *{    
     box-sizing: border-box;
     margin: 0;
@@ -15,9 +16,9 @@ const GlobalStyle = createGlobalStyle`
     -webkit-user-select: none;  /* Chrome all / Safari all */
     -moz-user-select: none;     /* Firefox all */
     -ms-user-select: none;      /* IE 10+ */
-    user-select: none;          /* Likely future */      
+    user-select: none;          /* Likely future */     
+    overflow-x: hidden ;
   }
-  ${fontsCSS}
 `;
 
 export default GlobalStyle;
