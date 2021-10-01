f80b21f209f2262b06e70da10e1720f30bb0aaa1
ValentinTT <valentintapiatorti@gmail.com>
2021-09-30 23:16:27 -0300

    Ading Netflix Sans Medium

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,11 +1,19 @@
 import { css } from "styled-components";
 import NetflixSans from "fonts/NetflixSans/NetflixSansRegular.ttf";
+import NetflixSansMedium from "fonts/NetflixSans/NetflixSansMedium.ttf";
 
 export const fontsCSS = css`
   @font-face {
     font-family: "NetflixSans";
     src: local("NetflixSans"), local("NetflixSans"),
       url(${NetflixSans}) format("truetype");
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: "NetflixSans Medium";
+    src: local("NetflixSans Medium"), local("NetflixSans Medium"),
+      url(${NetflixSansMedium}) format("truetype");
     font-weight: 300;
     font-style: normal;
   }
