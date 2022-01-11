65927f5d42f36dcda0b0d130b900f2b1c2f46e4e
ValentinTT <valentintapiatorti@gmail.com>
2022-01-11 16:02:12 -0300

    Adding fonts

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,6 +1,9 @@
 import { css } from "styled-components";
 import NetflixSans from "fonts/NetflixSans/NetflixSansRegular.ttf";
 import NetflixSansMedium from "fonts/NetflixSans/NetflixSansMedium.ttf";
+import NetflixSansLight from "fonts/NetflixSans/NetflixSansLight.ttf";
+import NetflixSansRegular from "fonts/NetflixSans/NetflixSansRegular.ttf";
+import NetflixSansThin from "fonts/NetflixSans/NetflixSansThin.ttf";
 
 export const fontsCSS = css`
   @font-face {
@@ -12,8 +15,32 @@ export const fontsCSS = css`
     font-display: block;
   }
   @font-face {
-    font-family: "NetflixSans Medium";
-    src: local("NetflixSans Medium"), local("NetflixSans Medium"),
+    font-family: "NetflixSansLight";
+    src: local("NetflixSansLight"), local("NetflixSansLight"),
+      url(${NetflixSansLight}) format("truetype");
+    font-weight: 200;
+    font-style: normal;
+    font-display: block;
+  }
+  @font-face {
+    font-family: "NetflixSansThin";
+    src: local("NetflixSansThin"), local("NetflixSansThin"),
+      url(${NetflixSansThin}) format("truetype");
+    font-weight: 200;
+    font-style: normal;
+    font-display: block;
+  }
+  @font-face {
+    font-family: "NetflixSansRegular";
+    src: local("NetflixSansRegular"), local("NetflixSansRegular"),
+      url(${NetflixSansRegular}) format("truetype");
+    font-weight: 200;
+    font-style: normal;
+    font-display: block;
+  }
+  @font-face {
+    font-family: "NetflixSansMedium";
+    src: local("NetflixSansMedium"), local("NetflixSansMedium"),
       url(${NetflixSansMedium}) format("truetype");
     font-weight: 200;
     font-style: normal;
